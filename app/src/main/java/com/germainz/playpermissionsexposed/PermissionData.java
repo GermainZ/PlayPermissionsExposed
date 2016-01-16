c2a375ba90060dae2f5ead191c8ae99c0c660d06
Carey Metcalfe <carey@cmetcalfe.ca>
2016-01-16 16:00:29 -0500

    Remove unused shortDescription from PermissionData class

--- app/src/main/java/com/germainz/playpermissionsexposed/PermissionData.java
+++ app/src/main/java/com/germainz/playpermissionsexposed/PermissionData.java
@@ -3,18 +3,14 @@ package com.germainz.playpermissionsexposed;
 public class PermissionData {
     public String permission;
     public String name;
-    public CharSequence shortDescription;
-    public CharSequence longDescription;
+    public CharSequence description;
 
-    public PermissionData(String permission, CharSequence name, CharSequence shortDescription, CharSequence longDescription) {
+    public PermissionData(String permission, CharSequence name, CharSequence description) {
         this.permission = permission;
-        if (name == null)
+        if (name == null || name.length() <= 0)
             this.name = permission;
-        else if (name.length() > 0)
-            this.name = ((String) name).substring(0, 1).toUpperCase() + ((String) name).substring(1);
         else
-            this.name = (String) name;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
+            this.name = ((String) name).substring(0, 1).toUpperCase() + ((String) name).substring(1);
+        this.description = description;
     }
 }
